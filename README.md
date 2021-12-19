a1c2b6e12979df3d1bb5b11834ecddb380419c4f
emilyshurtz94 <emily.shurtz94@gmail.com>
2021-12-19 10:56:11 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Weather Board Criteria
+
+* When a user searches for a city they are presented with the current and future weather conditions for that city, and the city is added to the search history
+
+* When a user sees the current weather conditions they are shown, city name, date, icon that represents the weather conditions, temperature, humidity, and UV index. 
+
+* When a user sees the UV index they are shown a color that indicates whether or not the conditions are favorable, moderate, or severe
+
+* When a user sees the future weather conditions they are shown a 5-day forecast that displays the date, icon representing the weather conditions, and the temperature.
+
+* When a user clicks on a city in the search history they are presented with current and future conditions for that city
+
+---
+
+## Screenshot of Weather Dashboard
+
+
+---
+
+### Github link 
+
+
+---
---
+++ script.js
@@ -0,0 +1,49 @@
+var apiKey= "c5d61dc25eb8f6eb71d75c8959de217b"
+
+// Form to search for a city
+    // Search button-- present with current and future conditions, save in history
+function currentWeather(){
+    // get user input
+   var userInput= $(this).parent().siblings(".input-group").children(".form-control").val().trim()
+   console.log(userInput)
+    // convert to lat and long
+    var geoURL= "http://api.openweathermap.org/geo/1.0/direct?q="+userInput+"&appid="+ apiKey
+    fetch(geoURL).then(function(response){ 
+        return response.json()
+    }).then(function(data){
+        console.log(data)
+        var lat= data[0].lat
+        var lon= data[0].lon
+        var apiURL= "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid="+apiKey+"&units=imperial"
+        fetch(apiURL).then(function(response){
+            return response.json()
+        }).then(function(data){
+            console.log(data)
+        })
+    })
+    // put into API
+}
+
+// Current weather conditions
+    // Name
+    // Date
+    // icon representing weather conditions
+    // temperature
+    // wind speed
+    // humidity
+    // UV Index-- color 
+        // Green= favorable
+        // yellow= moderate
+        // red=severe
+
+// Future Weather conditions
+    // 5-day forecast
+        // Date
+         // icon representing weather conditions
+        // temperature
+        // wind speed
+         // humidity
+
+// click on a city in search history
+    // Presented with current and future conditions 
+    $("#searchBtn").click(currentWeather)
