e23db291b567d3b21da5c59e988da46192c6a4a9
Emily Shurtz <emily@Emilys-MacBook-Pro.local>
2021-12-20 23:42:32 -0700

    update 2

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var apiKey= "c5d61dc25eb8f6eb71d75c8959de217b"
-
 // Form to search for a city
     // Search button-- present with current and future conditions, save in history
 function currentWeather(){
@@ -18,12 +17,18 @@ function currentWeather(){
         fetch(apiURL).then(function(response){
             return response.json()
         }).then(function(data){
+            localStorage.setItem("name",data)
             console.log(data)
         })
     })
     // put into API
 }
 
+for( var i=0;i<localStorage.length;i++)
+var currentCity= localStorage.getItem(i);
+var currentCityName= $("#city-name");
+currentCityName.append("#city-name");
+console.log(currentCityName)
 // Current weather conditions
     // Name
     // Date
